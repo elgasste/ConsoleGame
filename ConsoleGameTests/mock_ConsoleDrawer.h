1cb4f06d2e3510871450c074a14dd43b67b2d6bf
Steve <s.elgas@techsmith.com>
2022-11-12 15:03:23 -0500

    Added mock_ConsoleDrawer class

---
+++ ConsoleGameTests/mock_ConsoleDrawer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <gmock/gmock.h>
+#include <string>
+
+#include <ConsoleGame/IConsoleDrawer.h>
+
+class mock_ConsoleDrawer : public ConsoleGame::IConsoleDrawer
+{
+public:
+   MOCK_METHOD( void, Initialize, ( ), ( override ) );
+   MOCK_METHOD( void, CleanUp, ( ), ( override ) );
+
+   MOCK_METHOD( void, SetDefaultForegroundColor, ( ConsoleGame::ConsoleColor ), ( override ) );
+   MOCK_METHOD( void, SetDefaultBackgroundColor, ( ConsoleGame::ConsoleColor ), ( override ) );
+   MOCK_METHOD( void, ClearDrawBuffer, ( ), ( override ) );
+
+   MOCK_METHOD( void, Draw, ( int, int, char ), ( override ) );
+   MOCK_METHOD( void, Draw, ( int, int, char, ConsoleGame::ConsoleColor ), ( override ) );
+   MOCK_METHOD( void, Draw, ( int, int, char, ConsoleGame::ConsoleColor, ConsoleGame::ConsoleColor ), ( override ) );
+   MOCK_METHOD( void, Draw, ( int, int, const std::string& ), ( override ) );
+   MOCK_METHOD( void, Draw, ( int, int, const std::string&, ConsoleGame::ConsoleColor ), ( override ) );
+   MOCK_METHOD( void, Draw, ( int, int, const std::string&, ConsoleGame::ConsoleColor, ConsoleGame::ConsoleColor ), ( override ) );
+
+   MOCK_METHOD( void, FlipDrawBuffer, ( ), ( override ) );
+};
