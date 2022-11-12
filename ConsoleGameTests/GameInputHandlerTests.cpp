8fa1674404dc8a13066a3712c2846a0af2e4c425
Steve <s.elgas@techsmith.com>
2022-11-12 13:29:53 -0500

    Use NiceMocks in GameInputHandlerTests
    Wrapping mock objects in NiceMock stops the test runner from showing warnings on 'uninteresting' calls to mock objects. Finally! Turns out mock objects are 'naggy' by default, I hope that changes at some point.

--- ConsoleGameTests/GameInputHandlerTests.cpp
+++ ConsoleGameTests/GameInputHandlerTests.cpp
@@ -21,16 +21,16 @@ class GameInputHandlerTests : public Test
 public:
    void SetUp() override
    {
-      _inputReaderMock.reset( new mock_GameInputReader() );
-      _stateProviderMock.reset( new mock_GameStateProvider );
-      _eventAggregatorMock.reset( new mock_GameEventAggregator );
-      _startupInputHandler.reset( new mock_GameInputHandler );
+      _inputReaderMock.reset( new NiceMock<mock_GameInputReader> );
+      _stateProviderMock.reset( new NiceMock<mock_GameStateProvider> );
+      _eventAggregatorMock.reset( new NiceMock<mock_GameEventAggregator> );
+      _startupInputHandlerMock.reset( new NiceMock<mock_GameInputHandler> );
 
       _inputHandler.reset( new GameInputHandler( _inputReaderMock,
                                                  _stateProviderMock,
                                                  _eventAggregatorMock ) );
 
-      _inputHandler->AddInputHandlerForGameState( GameState::Startup, _startupInputHandler );
+      _inputHandler->AddInputHandlerForGameState( GameState::Startup, _startupInputHandlerMock );
 
       ON_CALL( *_stateProviderMock, GetGameState() ).WillByDefault( Return( GameState::Startup ) );
       ON_CALL( *_inputReaderMock, WasButtonPressed( _ ) ).WillByDefault( Return( false ) );
@@ -40,48 +40,43 @@ protected:
    shared_ptr<mock_GameInputReader> _inputReaderMock;
    shared_ptr<mock_GameStateProvider> _stateProviderMock;
    shared_ptr<mock_GameEventAggregator> _eventAggregatorMock;
-   shared_ptr<mock_GameInputHandler> _startupInputHandler;
+   shared_ptr<mock_GameInputHandler> _startupInputHandlerMock;
 
    shared_ptr<GameInputHandler> _inputHandler;
 };
 
-TEST_F( GameInputHandlerTests, HandleInput_Always_ReadsInputAndHandlesIt )
+TEST_F( GameInputHandlerTests, HandleInput_Always_ReadsInput )
 {
    EXPECT_CALL( *_inputReaderMock, ReadInput() );
-   EXPECT_CALL( *_inputReaderMock, WasButtonPressed( GameButton::Diagnostics ) );
-   EXPECT_CALL( *_stateProviderMock, GetGameState() );
-   EXPECT_CALL( *_startupInputHandler, HandleInput() );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( GameInputHandlerTests, HandleInput_DiagnosticsButtonWasNotPressed_DoesNotRaiseAnyEvents )
 {
-   EXPECT_CALL( *_inputReaderMock, ReadInput() );
-   EXPECT_CALL( *_inputReaderMock, WasButtonPressed( GameButton::Diagnostics ) );
    EXPECT_CALL( *_eventAggregatorMock, RaiseEvent( _ ) ).Times( 0 );
-   EXPECT_CALL( *_stateProviderMock, GetGameState() );
-   EXPECT_CALL( *_startupInputHandler, HandleInput() );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( GameInputHandlerTests, HandleInput_DiagnosticsButtonWasPressed_RaisesToggleDiagnosticsEvent )
 {
-   EXPECT_CALL( *_inputReaderMock, ReadInput() );
-   EXPECT_CALL( *_inputReaderMock, WasButtonPressed( GameButton::Diagnostics ) ).WillRepeatedly ( Return( true ) );
-   EXPECT_CALL( *_eventAggregatorMock, RaiseEvent( GameEvent::ToggleDiagnostics ) ).Times( 1 );
-   EXPECT_CALL( *_stateProviderMock, GetGameState() );
-   EXPECT_CALL( *_startupInputHandler, HandleInput() );
+   EXPECT_CALL( *_inputReaderMock, WasButtonPressed( GameButton::Diagnostics ) ).WillOnce( Return( true ) );
+   EXPECT_CALL( *_eventAggregatorMock, RaiseEvent( GameEvent::ToggleDiagnostics ) );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( GameInputHandlerTests, HandleInput_InputHandlerNotFoundForState_ThrowsException )
 {
-   EXPECT_CALL( *_inputReaderMock, ReadInput() );
-   EXPECT_CALL( *_inputReaderMock, WasButtonPressed( GameButton::Diagnostics ) );
    EXPECT_CALL( *_stateProviderMock, GetGameState() ).WillRepeatedly( Return( GameState::Playing ) );
 
    EXPECT_THROW( _inputHandler->HandleInput(), std::out_of_range );
+}
+
+TEST_F( GameInputHandlerTests, HandleInput_InputHandlerFoundForState_HandlesInputForState )
+{
+   EXPECT_CALL( *_startupInputHandlerMock, HandleInput() );
+
+   _inputHandler->HandleInput();
 }
