73b31379e5e9ce5cafc43a510e3f16dfcd8f2d23
Steve <s.elgas@techsmith.com>
2022-11-12 13:45:33 -0500

    Use NiceMocks in PlayingStateInputHandlerTests

--- ConsoleGameTests/PlayingStateInputHandlerTests.cpp
+++ ConsoleGameTests/PlayingStateInputHandlerTests.cpp
@@ -18,84 +18,66 @@ class PlayingStateInputHandlerTests : public Test
 public:
    void SetUp() override
    {
-      _inputReader.reset( new mock_GameInputReader() );
-      _commandExecutor.reset( new mock_GameCommandExecutor() );
+      _inputReaderMock.reset( new NiceMock<mock_GameInputReader> );
+      _commandExecutorMock.reset( new NiceMock<mock_GameCommandExecutor> );
 
-      _inputHandler.reset( new PlayingStateInputHandler( _inputReader,
-                                                         _commandExecutor ) );
+      _inputHandler.reset( new PlayingStateInputHandler( _inputReaderMock,
+                                                         _commandExecutorMock ) );
+
+      ON_CALL( *_inputReaderMock, WasButtonPressed( _ ) ).WillByDefault( Return( false ) );
+      ON_CALL( *_inputReaderMock, IsButtonDown( _ ) ).WillByDefault( Return( false ) );
    }
 
 protected:
-   shared_ptr<mock_GameInputReader> _inputReader;
-   shared_ptr<mock_GameCommandExecutor> _commandExecutor;
+   shared_ptr<mock_GameInputReader> _inputReaderMock;
+   shared_ptr<mock_GameCommandExecutor> _commandExecutorMock;
 
    shared_ptr<PlayingStateInputHandler> _inputHandler;
 };
 
 TEST_F( PlayingStateInputHandlerTests, HandleInput_AButtonWasPressed_ExecutesQuitCommand )
 {
-   EXPECT_CALL( *_inputReader, WasButtonPressed( GameButton::A ) ).WillOnce( Return( true ) );
-
-   EXPECT_CALL( *_commandExecutor, ExecuteCommand( GameCommand::Quit ) );
+   ON_CALL( *_inputReaderMock, WasButtonPressed( GameButton::A ) ).WillByDefault( Return( true ) );
+   EXPECT_CALL( *_commandExecutorMock, ExecuteCommand( GameCommand::Quit ) );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( PlayingStateInputHandlerTests, HandleInput_LeftButtonIsDown_ExecutesMovePlayerLeftCommand )
 {
-   EXPECT_CALL( *_inputReader, WasButtonPressed( GameButton::A ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Left ) ).WillOnce( Return( true ) );
-
-   EXPECT_CALL( *_commandExecutor, ExecuteCommand( GameCommand::MovePlayerLeft ) );
+   ON_CALL( *_inputReaderMock, IsButtonDown( GameButton::Left ) ).WillByDefault( Return( true ) );
+   EXPECT_CALL( *_commandExecutorMock, ExecuteCommand( GameCommand::MovePlayerLeft ) );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( PlayingStateInputHandlerTests, HandleInput_UpButtonIsDown_ExecutesMovePlayerUpCommand )
 {
-   EXPECT_CALL( *_inputReader, WasButtonPressed( GameButton::A ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Left ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Up ) ).WillOnce( Return( true ) );
-
-   EXPECT_CALL( *_commandExecutor, ExecuteCommand( GameCommand::MovePlayerUp ) );
+   ON_CALL( *_inputReaderMock, IsButtonDown( GameButton::Up ) ).WillByDefault( Return( true ) );
+   EXPECT_CALL( *_commandExecutorMock, ExecuteCommand( GameCommand::MovePlayerUp ) );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( PlayingStateInputHandlerTests, HandleInput_RightButtonIsDown_ExecutesMovePlayerRightCommand )
 {
-   EXPECT_CALL( *_inputReader, WasButtonPressed( GameButton::A ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Left ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Up ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Right ) ).WillOnce( Return( true ) );
-
-   EXPECT_CALL( *_commandExecutor, ExecuteCommand( GameCommand::MovePlayerRight ) );
+   ON_CALL( *_inputReaderMock, IsButtonDown( GameButton::Right ) ).WillByDefault( Return( true ) );
+   EXPECT_CALL( *_commandExecutorMock, ExecuteCommand( GameCommand::MovePlayerRight ) );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( PlayingStateInputHandlerTests, HandleInput_DownButtonIsDown_ExecutesMovePlayerDownCommand )
 {
-   EXPECT_CALL( *_inputReader, WasButtonPressed( GameButton::A ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Left ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Up ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Right ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Down ) ).WillOnce( Return( true ) );
-
-   EXPECT_CALL( *_commandExecutor, ExecuteCommand( GameCommand::MovePlayerDown ) );
+   ON_CALL( *_inputReaderMock, IsButtonDown( GameButton::Down ) ).WillByDefault( Return( true ) );
+   EXPECT_CALL( *_commandExecutorMock, ExecuteCommand( GameCommand::MovePlayerDown ) );
 
    _inputHandler->HandleInput();
 }
 
 TEST_F( PlayingStateInputHandlerTests, HandleInput_NoButtonsDown_DoesNotExecuteAnyCommand )
 {
-   EXPECT_CALL( *_inputReader, WasButtonPressed( GameButton::A ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Left ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Up ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Right ) ).WillOnce( Return( false ) );
-   EXPECT_CALL( *_inputReader, IsButtonDown( GameButton::Down ) ).WillOnce( Return( false ) );
-
-   EXPECT_CALL( *_commandExecutor, ExecuteCommand( _ ) ).Times( 0 );
+   EXPECT_CALL( *_commandExecutorMock, ExecuteCommand( _ ) ).Times( 0 );
 
    _inputHandler->HandleInput();
 }
