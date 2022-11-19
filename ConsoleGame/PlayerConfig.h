3fb45c4bfd7baa61e0acc9a915d90b0e3b99006a
Steve <s.elgas@techsmith.com>
2022-11-19 11:07:10 -0500

    Added PlayerConfig class

---
+++ ConsoleGame/PlayerConfig.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace ConsoleGame
+{
+   class PlayerConfig
+   {
+   public:
+      int StartPositionX;
+      int StartPositionY;
+
+      int StartVelocityX;
+      int StartVelocityY;
+
+      int MaxVelocityX;
+      int MaxVelocityY;
+
+      int VelocityDeltaX;
+      int VelocityDeltaY;
+   };
+}
