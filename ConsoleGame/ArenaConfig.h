2d4fe500e9c6b924fec65890d16b065fb412d6a6
Steve <s.elgas@techsmith.com>
2022-11-28 14:55:50 -0500

    Added constructor to ArenaConfig
    I'm not sure why this wasn't throwing static analysis warnings, but I thought it should be done anyway.

--- ConsoleGame/ArenaConfig.h
+++ ConsoleGame/ArenaConfig.h
@@ -5,6 +5,12 @@ namespace ConsoleGame
    class ArenaConfig
    {
    public:
+      ArenaConfig() :
+         Width( 0 ),
+         Height( 0 ),
+         PlayerStartX( 0 ),
+         PlayerStartY( 0 ) { }
+
       double Width;
       double Height;
 
