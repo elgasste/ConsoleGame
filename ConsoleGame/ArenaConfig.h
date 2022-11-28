c422f5d95be04b224df4e09e19f47e609762ff59
Steve <s.elgas@techsmith.com>
2022-11-28 15:12:12 -0500

    Whoops, missed this one...

--- ConsoleGame/ArenaConfig.h
+++ ConsoleGame/ArenaConfig.h
@@ -6,10 +6,10 @@ namespace ConsoleGame
    {
    public:
       ArenaConfig() :
-         Width( 0 ),
-         Height( 0 ),
-         PlayerStartX( 0 ),
-         PlayerStartY( 0 ) { }
+         Width( 0. ),
+         Height( 0. ),
+         PlayerStartX( 0. ),
+         PlayerStartY( 0. ) { }
 
       double Width;
       double Height;
