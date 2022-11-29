5f1c8cfe2fb018b25b5b3d8de63b0fd866a1a53a
Steve <s.elgas@techsmith.com>
2022-11-29 14:22:26 -0500

    Added constructor for PlayerConfig
    Not sure again why static analysis didn't pick this up, but I guess it should match all the other struct constructors.

--- ConsoleGame/PlayerConfig.h
+++ ConsoleGame/PlayerConfig.h
@@ -7,6 +7,13 @@ namespace ConsoleGame
    class PlayerConfig
    {
    public:
+      PlayerConfig() :
+         StartVelocityX( 0. ),
+         StartVelocityY( 0. ),
+         MaxVelocity( 0. ),
+         AccelerationPerSecond( 0. ),
+         StartDirection( (Direction)0 ) { }
+
       double StartVelocityX;
       double StartVelocityY;
 
